d06a4353405207b5e3b06eaa08203b75557b77bf
hlin <Zxsaqw12@&$>
2020-03-10 12:25:22 -0400

    Add test

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -9,4 +9,38 @@ public class Sneaker {
     private int size;
     private int qty;
     private float price;
+
+    public Sneaker(int id, String name, String brand, String sport, int qty, float price) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.sport = sport;
+        this.qty = qty;
+        this.price = price;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getSport() {
+        return this.sport;
+    }
+
+
+    public int getQty() {
+        return this.qty;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getName() { return this.name ; }
 }
